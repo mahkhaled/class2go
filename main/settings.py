b32cb441e8d96c257f70cb61274812ebeed2649d
Mahmoud <mahmoud.khaled@badrit.com>
2012-12-23 21:40:08 +0200

    merge

--- main/settings.py
+++ main/settings.py
@@ -101,7 +101,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/opt/' + APP + '/static/'
+STATIC_ROOT = '/home/mahmoud/DevProjects/class2go/class2go/main/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
