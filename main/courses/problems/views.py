c0e8b44baaad38f04be852bd267cd1147e63010a
Khaled <khaled.hady.mail@gmail.com>
2013-03-18 21:54:38 +0200

    authenticate

--- main/courses/problems/views.py
+++ main/courses/problems/views.py
@@ -6,13 +6,14 @@ from c2g.models import *
 from courses.common_page_data import get_common_page_data
 from courses.actions import auth_view_wrapper, auth_is_course_admin_view_wrapper
 
+@auth_view_wrapper
 def view(request, course_prefix, course_suffix, assignment_id, problem_id):
    try:
       common_page_data = get_common_page_data(request, course_prefix, course_suffix)
    except:
       raise Http404
    assignment = Assignment.objects.get(pk=assignment_id)
-   problem = Problem.objects.get(pk=problem_id)
+   problem = assignment.contest.problem_set.get(pk=problem_id)
    user = request.user
    team = Team.objects.getByUser(user)
    submissions = problem.submission_set.getByTeam(team)
